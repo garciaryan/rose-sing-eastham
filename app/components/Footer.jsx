3329a594cfce28104027f8456cf8dc1c1ed53fb2
Ryan Garcia <ryangarcia@Ryans-Mac-mini.attlocal.net>
2024-05-11 14:01:58 -0700

    init commit - header and footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white h-32 w-full text-black relative flex items-center justify-center flex-col">
+        <strong>Rose Sing Eastham and Associates, Inc.</strong>
+        <div>Electrical Consultants</div>
+        <div>131 S. Dunworth St.</div>
+        <div>Visalia, CA, 93292-6705</div>
+    </footer>
+  );
+}
---
+++ app/components/Header.jsx
@@ -0,0 +1,91 @@
+'use client';
+
+import {
+  Navbar, 
+  NavbarBrand, 
+  NavbarContent, 
+  NavbarItem, 
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
+import Image from "next/image";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faCity, faBolt, faUsers, faHandshake } from "@fortawesome/free-solid-svg-icons";
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const fontStyle = {
+    fontSize: '20px',
+    width: '25px',
+    marginRight: '8px'
+  }
+
+  const mobileMenuItems = [
+    { text: <><FontAwesomeIcon style={fontStyle} icon={faUsers} /><div>About Us</div></>, url: '/about-us' },
+    { text: <><FontAwesomeIcon style={fontStyle} icon={faBolt} /><div>Services</div></>, url: '/services' },
+    { text: <><FontAwesomeIcon style={fontStyle} icon={faCity} /><div>Projects</div></>, url: '/' },
+    { text: <><FontAwesomeIcon style={fontStyle} icon={faEnvelope} /><div>Contact Us</div></>, url: '/contact-us' },
+    { text: <><FontAwesomeIcon style={fontStyle} icon={faHandshake} /><div>Join the Team</div></>, url: '/careers' }
+  ];
+
+  const desktopMenuItems = [
+    { text: 'About Us', url: '/about-us' },
+    { text: 'Services', url: '/services' },
+    { text: 'Projects', url: '/' },
+    { text: 'Contact Us', url: '/contact-us' },
+    { text: 'Join the Team', url: '/careers' },
+  ]
+
+  return (
+    <Navbar
+      shouldHideOnScroll
+      disableAnimation
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      className="h-24 bg-white"
+    >
+      <NavbarContent className="pr-3 sm:pr-0" justify="center">
+        <NavbarBrand>
+          <Image
+            src="/rse-logo.png"
+            alt="Rose Sing Eastham and Associates, Inc. logo"
+            width={100}
+            height={79}
+          />
+          <p className="text-xl ml-2 text-[#4F6D7A]">Rose Sing Eastham <br />and Associates, Inc.</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden lg:flex gap-4" justify="center">
+        {desktopMenuItems.map((item, index) => (
+          <NavbarItem key={`${item}=${index}`}>
+            <a className="text-black hover:text-[#4F6D7A]" href={item.url}>
+              {item.text}
+            </a>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent className="lg:hidden" justify="end">
+        <NavbarMenuToggle
+          style={{ color: '#000000' }}
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="lg:hidden"
+        />
+      </NavbarContent>
+      <NavbarMenu className="bg-[#C0D6DF] top-24">
+        {mobileMenuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`} className="pt-5">
+            <a
+              className="w-full text-black flex items-center"
+              href={item.url}
+            >
+              {item.text}
+            </a>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
