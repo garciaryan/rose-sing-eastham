6c29e87d83b9a24eec2572d8fedbadbef472bbc2
Ryan Garcia <ryangarcia@Macmini.attlocal.net>
2025-07-10 21:31:51 -0700

    adds robots.txt file

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ import Head from "next/head";
 const montserrat = Montserrat({ subsets: ['latin'] });
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
-export const metadata = {
+const metadata = {
   title: "Rose Sing Eastham and Associates, Inc. | Electrical Consultants",
   description: "Rose Sing Eastham and Associates, Inc. is located in Visalia, CA and performs projects throughout the Central and Northern California areas. Our staff of engineering, design, inspection, and CAD professionals can support projects of varying types and sizes.",
 };
